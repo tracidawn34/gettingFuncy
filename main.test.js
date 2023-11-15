cd11bbf86f5addb5977631634bff109d5083de50
unknown <tmcfall.prsvr@gmail.com>
2023-11-15 11:39:58 -0500

    final

--- main.test.js
+++ main.test.js
@@ -1,8 +1,30 @@
 /**********************************
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
-
-
+let x = 3
+let y = 10
+let z = 4
+let greeting = "Hello"
+let firstName = "Traci"
+let callStatus = "ringing"
+function double(){
+  x = x * 2
+}
+function triple(){
+  y = y * 3
+}
+function square(){
+  z = z ** 2
+}
+function beNice(){
+  greeting = "hello" + firstName
+}
+function changeName(){
+  firstName = 'Danger'
+}
+function hangUp(){
+callStatus = 'call ended'
+}
 
 /*********************************************************************************************************************
 
